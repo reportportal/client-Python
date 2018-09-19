d8773d22ce8ebda0ee8c2f0f854192a8a2710364
Joshua Moravec <jmoravec@carbonblack.com>
2018-09-19 15:54:18 -0500

    Add verify_ssl option

--- reportportal_client/service_async.py
+++ reportportal_client/service_async.py
@@ -118,7 +118,8 @@ class ReportPortalServiceAsync(object):
 
     def __init__(self, endpoint, project, token, api_base="api/v1",
                  error_handler=None, log_batch_size=20,
-                 is_skipped_an_issue=True):
+                 is_skipped_an_issue=True,
+                 verify_ssl=True):
         """Init the service class.
 
         Args:
@@ -130,12 +131,13 @@ class ReportPortalServiceAsync(object):
                 during items processing (in thread)
             is_skipped_an_issue: option to mark skipped tests as not
                 'To Investigate' items on Server side.
+            verify_ssl: option to not verify ssl certificates
         """
         super(ReportPortalServiceAsync, self).__init__()
         self.error_handler = error_handler
         self.log_batch_size = log_batch_size
         self.rp_client = ReportPortalService(
-            endpoint, project, token, api_base, is_skipped_an_issue)
+            endpoint, project, token, api_base, is_skipped_an_issue, verify_ssl)
         self.log_batch = []
         self.supported_methods = ["start_launch", "finish_launch",
                                   "start_test_item", "finish_test_item", "log"]
