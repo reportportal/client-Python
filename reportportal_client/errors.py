c1e643640f23c79beddadeb20009d98736dcc233
Dzmitry Humianiuk <dzmitry_humianiuk@epam.com>
2018-10-04 13:50:30 +0300

    Update errors.py

--- reportportal_client/errors.py
+++ reportportal_client/errors.py
@@ -1,4 +1,4 @@
-#  Copyright http://EPAM.com
+#  Copyright (c) 2018 http://reportportal.io 
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
