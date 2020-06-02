b1c0f9affc53acf0a707d861851f7ed5981fcbc2
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2020-06-02 13:29:23 +0300

    Fixing review comments

--- tests/test_service.py
+++ tests/test_service.py
@@ -24,40 +24,40 @@ class TestServiceFunctions:
 
     def test_check_convert_to_string(self):
         """Test for support and convert strings to utf-8."""
-        expect(_convert_string("Hello world") == 'Hello world')
-        expect(lambda: isinstance(_convert_string("Hello world"), str))
+        expect(_convert_string('Hello world') == 'Hello world')
+        expect(lambda: isinstance(_convert_string('Hello world'), str))
         assert_expectations()
 
     @pytest.mark.parametrize('system', [True, False])
     def test_dict_to_payload_with_system_key(self, system):
         """Test convert dict to list of dicts with key system."""
-        initial_dict = {"aa": 1, "b": 2, "system": system}
+        initial_dict = {'aa': 1, 'b': 2, 'system': system}
         expected_list = [{'key': 'aa', 'value': '1', 'system': system},
                          {'key': 'b', 'value': '2', 'system': system}]
         assert _dict_to_payload(initial_dict) == expected_list
 
     def test_get_id(self, response):
         """Test for the get_id function."""
-        assert _get_id(response(200, {"id": 123})) == 123
+        assert _get_id(response(200, {'id': 123})) == 123
 
     def test_get_msg(self, response):
         """Test for the get_msg function."""
-        fake_json = {"id": 123}
+        fake_json = {'id': 123}
         assert _get_msg(response(200, fake_json)) == fake_json
 
     def test_get_data(self, response):
         """Test for the get_data function."""
-        fake_json = {"id": 123}
+        fake_json = {'id': 123}
         assert _get_data(response(200, fake_json)) == fake_json
 
     def test_get_json(self, response):
         """Test for the get_json function."""
-        fake_json = {"id": 123}
+        fake_json = {'id': 123}
         assert _get_json(response(200, fake_json)) == fake_json
 
     def test_get_messages(self):
         """Test for the get_messages function."""
-        data = {"responses": [{"errorCode": 422, "message": "error"}]}
+        data = {'responses': [{'errorCode': 422, 'message': 'error'}]}
         assert _get_messages(data) == ['422: error']
 
 
@@ -71,7 +71,7 @@ class TestReportPortalService:
         :param mock_get:   Mocked _get_data() function
         :param rp_service: Pytest fixture
         """
-        mock_get.return_value = {"id": 111}
+        mock_get.return_value = {'id': 111}
         launch_id = rp_service.start_launch('name', datetime.now().isoformat())
         assert launch_id == 111
 
@@ -82,10 +82,10 @@ class TestReportPortalService:
         :param mock_get:   Mocked _get_msg() function
         :param rp_service: Pytest fixture
         """
-        mock_get.return_value = {"id": 111}
+        mock_get.return_value = {'id': 111}
         _get_msg = rp_service.finish_launch(
             'name', datetime.now().isoformat())
-        assert _get_msg == {"id": 111}
+        assert _get_msg == {'id': 111}
 
     @mock.patch('platform.system', mock.Mock(return_value='linux'))
     @mock.patch('platform.machine', mock.Mock(return_value='Windows-PC'))
@@ -132,14 +132,14 @@ class TestReportPortalService:
                 == expected_result)
         assert cond
 
-    @mock.patch("reportportal_client.service._get_data",
-                mock.Mock(return_value={"id": 123}))
+    @mock.patch('reportportal_client.service._get_data',
+                mock.Mock(return_value={'id': 123}))
     def test_start_item(self, rp_service):
         """Test for validate start_test_item.
 
         :param: rp_service: fixture of ReportPortal
         """
-        rp_start = rp_service.start_test_item(name="name",
+        rp_start = rp_service.start_test_item(name='name',
                                               start_time=1591032041348,
                                               item_type='STORY')
         expected_result = dict(json={'name': 'name',
@@ -157,33 +157,33 @@ class TestReportPortalService:
         assert rp_start == 123
 
     start_item_optional = [
-        ('code_ref', '/path/to/test - test_item', "codeRef",
+        ('code_ref', '/path/to/test - test_item', 'codeRef',
          '/path/to/test - test_item'),
-        ('attributes', {'attr1': True}, "attributes",
+        ('attributes', {'attr1': True}, 'attributes',
          [{'key': 'attr1', 'value': 'True', 'system': False}])
     ]
 
     @pytest.mark.parametrize(
-        "field_name,field_value,expected_name,expected_value",
+        'field_name,field_value,expected_name,expected_value',
         start_item_optional)
-    @mock.patch("reportportal_client.service._get_data",
-                mock.Mock(return_value={"id": 123}))
+    @mock.patch('reportportal_client.service._get_data',
+                mock.Mock(return_value={'id': 123}))
     def test_start_item_code_optional_params(self, rp_service, field_name,
                                              field_value, expected_name,
                                              expected_value):
         """Test for validate different fields in start_test_item.
 
-        :param: rp_service: fixture of ReportPortal
-        :param: field_name a name of a field bypassed to
+        :param: rp_service:     fixture of ReportPortal
+        :param: field_name:     a name of a field bypassed to
         rp_service.start_test_item method
-        :param: field_value a value of a  field bypassed to
+        :param: field_value:    a value of a  field bypassed to
         rp_service.start_test_item method
-        :param: expected_name a name of a field which should be in the result
+        :param: expected_name:  a name of a field which should be in the result
         JSON request
-        :param: expected_value an exact value of a field which should be in the
-        result JSON request
+        :param: expected_value: an exact value of a field which should be in
+        the result JSON request
         """
-        rp_service.start_test_item(name="name", start_time=1591032041348,
+        rp_service.start_test_item(name='name', start_time=1591032041348,
                                    item_type='STORY',
                                    **{field_name: field_value})
         expected_result = dict(json={'name': 'name',
