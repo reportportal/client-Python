71b61c240cb82c38916a6977192e01dda5fbfea0
Pakhalchuk Nikita <pahniki@gmail.com>
2020-10-12 01:30:01 +0300

    Integrate test manager with test items

--- reportportal_client/core/test_manager.py
+++ reportportal_client/core/test_manager.py
@@ -14,20 +14,18 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-
-from pprint import pprint
-from random import randint
-from time import time
-from weakref import ref
-
 import six
+
+from reportportal_client.helpers import generate_uuid
+from reportportal_client.items.rp_log_items.rp_log_item import RPLogItem
+from reportportal_client.items.rp_test_items.rp_child_test_item import \
+    RPChildTestItem
+from reportportal_client.items.rp_test_items.rp_root_test_item import \
+    RPRootTestItem
 
 
 # todo: add logging
-# todo: update logic for log items
 # todo: add type hinting (pyi)
-# todo: remove testing code
-# todo: [optional] add unittests
 
 
 def _convert_string(value):
@@ -55,55 +53,6 @@ def _dict_to_payload(dictionary):
     ]
 
 
-# Todo: draft class for testing purpose
-class BaseItem(object):
-    def __init__(self, weight, data):
-        self.uuid = str(randint(0, 1000))
-        self.data = data
-        self.weight = weight
-
-        self.parent = None
-
-        self.request = None
-        self.response = None
-
-    def set_parent(self, parent):
-        self.parent = ref(parent)
-
-
-# Todo: draft class for testing purpose
-class TestItem(BaseItem):
-    def __init__(self, name, item_type, data):
-        super(TestItem, self).__init__(weight=None, data=data)
-        self.name = name
-        self.type = item_type
-
-        self.children = []
-
-        self.request = None
-        self.response = None
-
-    def add_child(self, child):
-        child.set_parent(self)
-        self.children.append(child)
-
-    def update(self, data):
-        self.data.update(data)
-
-    def finish(self, data):
-        self.data.update(data)
-
-    def remove(self):
-        # Todo: Remove children chain
-        self.parent and self.parent().children.remove(self)
-
-
-# Todo: draft class for testing purpose
-class LogItem(BaseItem):
-    def __init__(self, data):
-        super(LogItem, self).__init__(weight=None, data=data)
-
-
 class TestManager(object):
     """Manage test items during single launch.
 
@@ -121,24 +70,21 @@ class TestManager(object):
             }
     """
 
-    def __init__(self, launch_id):
-        self.launch_id = launch_id
-        self.__storage = []
+    def __init__(self, rp_url, session, api_version, launch_id, project_name):
+        """Initialize instance attributes.
 
-    def __add_test_item(self, test_item, parent_item_id=None):
-        """Create test item with given params.
-
-        :param test_item:       test item or log object
-        :param parent_item_id:  parent item uuid for given test item
-        :return: test item uuid
+        :param rp_url:        report portal url
+        :param session:       Session object
+        :param api_version:   RP API version
+        :param launch_id:   Parent launch UUID
+        :param project_name:  RP project name
         """
-        if not parent_item_id:
-            self.__storage.append(test_item)
-        else:
-            self.get_test_item(parent_item_id).add_child(test_item)
-
-        # Todo: wait for response to get item uuid
-        return test_item.uuid
+        self.rp_url = rp_url
+        self.session = session
+        self.api_version = api_version
+        self.launch_id = launch_id
+        self.project_name = project_name
+        self.__storage = []
 
     def start_test_item(self,
                         name,
@@ -171,19 +117,39 @@ class TestManager(object):
         if parameters:
             parameters = _dict_to_payload(parameters)
 
-        data = {
-            "name": name,
+        item_data = {
             "description": description,
             "attributes": attributes,
-            "startTime": start_time,
-            "launchUuid": self.launch_id,
-            "type": item_type,
             "parameters": parameters,
             "hasStats": has_stats
         }
-        kwargs and data.update(kwargs)
-        new_test_item = TestItem(name, item_type, data)
-        return self.__add_test_item(new_test_item, parent_item_id)
+        kwargs and item_data.update(kwargs)
+        uuid = generate_uuid()
+        if not parent_item_id:
+            test_item = RPRootTestItem(self.rp_url,
+                                       self.session,
+                                       self.api_version,
+                                       self.project_name,
+                                       name,
+                                       item_type,
+                                       self.launch_id,
+                                       uuid,
+                                       **item_data)
+            self.__storage.append(test_item)
+        else:
+            parent_item = self.get_test_item(parent_item_id)
+            test_item = RPChildTestItem(self.rp_url,
+                                        self.session,
+                                        self.api_version,
+                                        self.project_name,
+                                        parent_item,
+                                        name,
+                                        item_type,
+                                        self.launch_id,
+                                        uuid,
+                                        **item_data)
+        test_item.start(start_time)
+        return uuid
 
     def update_test_item(self, item_uuid, attributes=None, description=None,
                          **kwargs):
@@ -194,11 +160,8 @@ class TestManager(object):
         :param list attributes: test item attributes(tags)
                                 Pairs of key and value (see class doc string)
         """
-        data = {
-            "description": description,
-            "attributes": attributes,
-        }
-        self.get_test_item(item_uuid).update(data)
+        self.get_test_item(item_uuid)
+        raise NotImplementedError()
 
     def finish_test_item(self,
                          item_uuid,
@@ -207,7 +170,7 @@ class TestManager(object):
                          issue=None,
                          attributes=None,
                          **kwargs):
-        """Finish the test item.
+        """Finish test item.
 
         :param item_uuid:  id of the test item
         :param end_time:   time in UTC format
@@ -216,35 +179,24 @@ class TestManager(object):
         :param attributes: list of attributes
         :param kwargs:     other parameters
         """
-        # check if skipped test should not be marked as "TO INVESTIGATE"
+        # check if the test is skipped, if not - do not mark as TO INVESTIGATE
         if issue is None and status == "SKIPPED":
             issue = {"issue_type": "NOT_ISSUE"}
-
         if attributes and isinstance(attributes, dict):
             attributes = _dict_to_payload(attributes)
-
-        data = {
-            "endTime": end_time,
-            "status": status,
-            "issue": issue,
-            "launchUuid": self.launch_id,
-            "attributes": attributes
-        }
-        kwargs and data.update(kwargs)
-        self.get_test_item(item_uuid).finish(data)
+        self.get_test_item(item_uuid).finish(end_time, status, issue=issue,
+                                             attributes=attributes, **kwargs)
 
     def remove_test_item(self, item_uuid):
         """Remove test item by uuid.
 
         :param item_uuid: test item uuid
         """
-        test_item = self.get_test_item(item_uuid)
-        if test_item.parent:
-            test_item.remove()
-        else:
-            self.__storage.remove(test_item)
+        self.get_test_item(item_uuid)
+        raise NotImplementedError()
 
-    def log(self, time, message, level=None, attachment=None, item_id=None):
+    def log(self, time, message=None, level=None, attachment=None,
+            item_id=None):
         """Log message. Can be added to test item in any state.
 
         :param time:        log time
@@ -254,18 +206,16 @@ class TestManager(object):
         :param item_id:     parent item UUID
         :return:            log item UUID
         """
-        data = {
-            "launchUuid": self.launch_id,
-            "time": time,
-            "message": message,
-            "level": level,
-        }
-        if item_id:
-            data["itemUuid"] = item_id
-        elif attachment:
-            data["attachment"] = attachment
-
-        return self.__add_test_item(LogItem(data), parent_item_id=item_id)
+        uuid = generate_uuid()
+        # Todo: Do we store log items?
+        log_item = RPLogItem(self.rp_url,
+                             self.session,
+                             self.api_version,
+                             self.project_name,
+                             self.launch_id,
+                             uuid)
+        log_item.create(time, attachment, item_id, level, message)
+        return uuid
 
     def get_test_item(self, item_uuid):
         """Get test item by its uuid in the storage.
@@ -284,120 +234,15 @@ class TestManager(object):
         :param storage: list with test item objects
         :return: test item object if found else None
         """
-        for test_item in storage:
-            if item_uuid == test_item.uuid:
+        for test_item in reversed(storage):
+            if item_uuid == test_item.generated_id:
                 return test_item
             else:
-                if hasattr(test_item, "children") and test_item.children:
+                if hasattr(test_item, "child_items") and test_item.child_items:
                     found_item = self._find_item(item_uuid,
-                                                 test_item.children)
+                                                 test_item.child_items)
                     if found_item:
                         return found_item
 
-
-if __name__ == '__main__':
-    # Testing
-
-    def timestamp():
-        return str(int(time() * 1000))
-
-
-    launch_id = 'Lola launch'
-    tm = TestManager(launch_id)
-    ids = list()
-
-    print("\n\nCreate")
-    print("Test items structure:\n\\a\n\\b\n\t\\c\n\t\t\\d\n\\e\n")
-
-    ids.append(tm.start_test_item(name="Test Case a",
-                                  description="First Test Case",
-                                  tags=["Image", "Smoke"],
-                                  start_time=timestamp(),
-                                  item_type="SUITE",
-                                  parameters={"key1": "val1",
-                                              "key2": "val2"}))
-
-    ids.append(tm.start_test_item(name="Test Case b",
-                                  description="Test Case",
-                                  tags=["Image", "Smoke"],
-                                  start_time=timestamp(),
-                                  item_type="STEP",
-                                  parameters={"key1": "val1",
-                                              "key2": "val2"}))
-
-    ids.append(tm.start_test_item(name="Test Case c",
-                                  description="Test Case",
-                                  tags=["Image", "Smoke"],
-                                  start_time=timestamp(),
-                                  item_type="STEP",
-                                  parent_item_id=ids[1],
-                                  parameters={"key1": "val1",
-                                              "key2": "val2"}))
-
-    ids.append(tm.start_test_item(name="Test Case d",
-                                  description="Test Case",
-                                  tags=["Image", "Smoke"],
-                                  start_time=timestamp(),
-                                  item_type="STEP",
-                                  parent_item_id=ids[2],
-                                  parameters={"key1": "val1",
-                                              "key2": "val2"}))
-
-    ids.append(tm.start_test_item(name="Test Case e",
-                                  description="First Test Case",
-                                  tags=["Image", "Smoke"],
-                                  start_time=timestamp(),
-                                  item_type="SUITE",
-                                  parameters={"key1": "val1",
-                                              "key2": "val2"}))
-
-    print("ids:", [x for x in ids], "\n")
-
-
-    def foo(aaa):
-        print("\n")
-        print(aaa, aaa.uuid, aaa.name)
-        print(aaa.parent)
-        print(aaa.children)
-        pprint(aaa.data)
-
-
-    for x in ids:
-        foo(tm.get_test_item(x))
-
-    # Negative check:
-    assert tm.get_test_item('dsdsd') is None
-
-    print("\n\nUpdate")
-    tm.update_test_item(ids[1])
-    tm.update_test_item(ids[3])
-
-    foo(tm.get_test_item(ids[1]))
-    foo(tm.get_test_item(ids[3]))
-
-    print("\n\nFinish")
-    tm.finish_test_item(ids[3], timestamp(), "SKIPPED")
-    tm.finish_test_item(ids[2], timestamp(), "PASS")
-
-    foo(tm.get_test_item(ids[2]))
-    foo(tm.get_test_item(ids[3]))
-
-    print("\n\nLogs")
-    log_id_1 = tm.log(timestamp(), "running processes", "INFO")
-    log_id_2 = tm.log(timestamp(), "running processes", "ERROR", item_id=ids[3])
-
-    foo(tm.get_test_item(ids[3]))
-    log1 = tm.get_test_item(log_id_1)
-    print(log1, log1.uuid, log1.parent)
-    log2 = tm.get_test_item(log_id_2)
-    print(log2, log2.uuid, log2.parent)
-
-    print("\n\nRemove")
-    foo(tm.get_test_item(ids[2]))
-    tm.remove_test_item(ids[3])
-    assert tm.get_test_item(ids[3]) is None
-    foo(tm.get_test_item(ids[2]))
-
-    foo(tm.get_test_item(ids[0]))
-    tm.remove_test_item(ids[0])
-    assert tm.get_test_item(ids[0]) is None
+    def get_storage(self):
+        return self.__storage
