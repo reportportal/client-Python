79ee467c039f10bb4aa3e208849220d8e2bea0be
Ivan Ivanou <iivanou@ddn.com>
2020-10-29 15:04:58 +0300

    Implement sending events to GA

---
+++ reportportal_client/external/google_analytics.pyi
@@ -0,0 +1,10 @@
+from logging import Logger
+import requests
+from .constants import GA_ENDPOINT as GA_ENDPOINT, GA_INSTANCE as GA_INSTANCE
+from typing import Text
+
+logger: Logger
+
+def _get_client_info() -> tuple: ...
+
+def send_event(agent_name: Text, agent_version: Text) -> requests.Response: ...
