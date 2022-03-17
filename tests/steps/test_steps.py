c4951f85780d2f1734a9d702ca5d4ec7bb7c56e5
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2022-03-17 21:43:39 +0300

    First nested steps tests

---
+++ tests/steps/test_steps.py
@@ -0,0 +1,33 @@
+#  Copyright (c) 2022 https://reportportal.io .
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#  https://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License
+from reportportal_client import step
+
+NESTED_STEP_NAME = 'test nested step'
+PARENT_STEP_ID = '123-123-1234-123'
+NESTED_STEP_ID = '321-321-4321-321'
+
+
+def test_nested_steps_are_skipped_without_parent(rp_client):
+    with step(NESTED_STEP_NAME, rp_client=rp_client):
+        print("Hello nested steps")
+    assert rp_client.session.post.call_count == 0
+    assert rp_client.session.put.call_count == 0
+
+
+def test_nested_steps_reported_with_parent(rp_client):
+    rp_client.step_reporter.set_parent('STEP', PARENT_STEP_ID)
+
+    with step(NESTED_STEP_NAME, rp_client=rp_client):
+        print("Hello nested steps")
+    assert rp_client.session.post.call_count == 1
+    assert rp_client.session.put.call_count == 1
