ad0ce8f1a8bfe309d3bd4624760d63e9b13738db
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2022-03-23 16:33:26 +0300

    Pydocs add

--- reportportal_client/_local/__init__.py
+++ reportportal_client/_local/__init__.py
@@ -10,6 +10,7 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License
+"""Report Portal client context storing and retrieving module"""
 from threading import local
 
 
@@ -17,9 +18,18 @@ __INSTANCES = local()
 
 
 def current():
+    """Return current Report Portal client which is used by the current thread.
+    """
     if hasattr(__INSTANCES, 'current'):
         return __INSTANCES.current
 
 
 def set_current(client):
+    """Save Report Portal client as current.
+
+    The method is not intended to use used by users. Report Portal client calls
+    it itself when new client is created.
+
+    :param client: Report Portal client
+    """
     __INSTANCES.current = client
