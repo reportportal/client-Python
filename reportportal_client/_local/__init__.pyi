86163329b443b42fbbf71052be22375bda6c99fb
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2022-04-28 14:01:39 +0300

    Type annotations

--- reportportal_client/_local/__init__.pyi
+++ reportportal_client/_local/__init__.pyi
@@ -10,24 +10,13 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License
-"""Report Portal client context storing and retrieving module."""
-from threading import local
 
-__INSTANCES = local()
+from typing import Optional
 
-
-def current():
-    """Return current Report Portal client."""
-    if hasattr(__INSTANCES, 'current'):
-        return __INSTANCES.current
+from reportportal_client.client import RPClient
 
 
-def set_current(client):
-    """Save Report Portal client as current.
+def current() -> Optional[RPClient]: ...
 
-    The method is not intended to use used by users. Report Portal client calls
-    it itself when new client is created.
 
-    :param client: Report Portal client
-    """
-    __INSTANCES.current = client
+def set_current(client: Optional[RPClient]) -> None: ...
