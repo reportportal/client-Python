8041c64a24925d648b25d449489bb3a15cd3c727
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-01-09 12:51:07 +0300

    Minor format fix

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10'
+        'Programming Language :: Python :: 3.10',
     ],
     install_requires=read_file('requirements.txt').splitlines(),
 )
