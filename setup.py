22fafd184a177356138841fdc69e0ea6dcf65e3a
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-01-09 12:41:28 +0300

    Long description added

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
 """Config for setup package client Python."""
 
+import os
 from setuptools import setup, find_packages
 
 __version__ = '5.2.6'
 
-with open('requirements.txt') as f:
-    requirements = f.read().splitlines()
+
+def read_file(fname):
+    """Read the given file.
+
+    :param fname: Name of the file to be read
+    :return:      Output of the given file
+    """
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        return f.read()
+
 
 setup(
     name='reportportal-client',
     packages=find_packages(exclude=('tests', 'tests.*')),
     version=__version__,
     description='Python client for Report Portal v5.',
+    long_description=read_file('README.md'),
+    long_description_content_type='text/markdown',
     author_email='SupportEPMC-TSTReportPortal@epam.com',
     url='https://github.com/reportportal/client-Python',
     download_url=('https://github.com/reportportal/client-Python/'
@@ -26,5 +37,5 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10'
     ],
-    install_requires=requirements
+    install_requires=read_file('requirements.txt').splitlines(),
 )
