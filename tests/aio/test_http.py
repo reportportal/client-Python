22418a48752a1da0a1be3758e99348bdfb66b53d
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-09-28 22:35:14 +0300

    Add more tests

--- tests/aio/test_http.py
+++ tests/aio/test_http.py
@@ -25,14 +25,43 @@ from reportportal_client.aio.http import RetryingClientSession
 HTTP_TIMEOUT_TIME = 1.2
 
 
-class TimeoutHttpHandler(http.server.BaseHTTPRequestHandler):
+class OkHttpHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "application/json")
+        self.end_headers()
+        self.wfile.write('{}\n\n'.encode("utf-8"))
+        self.wfile.flush()
 
+
+class TimeoutHttpHandler(http.server.BaseHTTPRequestHandler):
     def do_GET(self):
         time.sleep(HTTP_TIMEOUT_TIME)
         self.send_response(200)
         self.send_header("Content-Type", "application/json")
         self.end_headers()
         self.wfile.write('{}\n\n'.encode("utf-8"))
+        self.wfile.flush()
+
+
+class ResetHttpHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.wfile.close()
+
+
+class ErrorHttpHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(500, 'Internal Server Error')
+        self.end_headers()
+        self.wfile.write('Internal Server Error\n\n'.encode("utf-8"))
+        self.wfile.flush()
+
+
+class ThrottlingHttpHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(429, 'Throttling')
+        self.end_headers()
+        self.wfile.write('Throttling\n\n'.encode("utf-8"))
         self.wfile.flush()
 
 
@@ -52,26 +81,69 @@ def get_http_server(server_class=SERVER_CLASS, server_address=SERVER_ADDRESS,
 
 @pytest.mark.skipif(sys.version_info < (3, 8),
                     reason="the test requires Python 3.8 or higher")
+@pytest.mark.parametrize(
+    'server_class, port, expected_delay, timeout_seconds, is_exception',
+    [
+        (TimeoutHttpHandler, 8001, 14.4, 1.0, True),
+        (ResetHttpHandler, 8002, 8.4, 1.0, True),
+        (ErrorHttpHandler, 8003, 1.1, 1.0, False),
+        (ThrottlingHttpHandler, 8004, 27.93, 1.0, False),
+    ]
+)
 @pytest.mark.asyncio
-async def test_retry_on_request_timeout():
-    timeout = aiohttp.ClientTimeout(connect=1, sock_read=1)
-    session = RetryingClientSession('http://localhost:8000', timeout=timeout, max_retry_number=5,
-                                    base_retry_delay=0.01)
+async def test_retry_on_request_error(server_class, port, expected_delay, timeout_seconds, is_exception):
+    retry_number = 5
+    timeout = aiohttp.ClientTimeout(connect=timeout_seconds, sock_read=timeout_seconds)
+    connector = aiohttp.TCPConnector(force_close=True)
+    session = RetryingClientSession(f'http://localhost:{port}', timeout=timeout,
+                                    max_retry_number=retry_number, base_retry_delay=0.01, connector=connector)
     parent_request = super(type(session), session)._request
     async_mock = mock.AsyncMock()
     async_mock.side_effect = parent_request
     exception = None
-    with get_http_server(server_handler=TimeoutHttpHandler):
+    result = None
+    with get_http_server(server_handler=server_class, server_address=('', port)):
         with mock.patch('reportportal_client.aio.http.ClientSession._request', async_mock):
             async with session:
                 start_time = time.time()
                 try:
-                    await session.get('/')
+                    result = await session.get('/')
                 except Exception as exc:
                     exception = exc
                 total_time = time.time() - start_time
-    retries_and_delays = 6 + 0.02 + 0.4 + 8
-    assert exception is not None
-    assert async_mock.call_count == 6
-    assert total_time > retries_and_delays
-    assert total_time < retries_and_delays * 1.5
+    if is_exception:
+        assert exception is not None
+    else:
+        assert exception is None
+        assert not result.ok
+    assert async_mock.call_count == 1 + retry_number
+    assert total_time > expected_delay
+    assert total_time < expected_delay * 1.5
+
+
+@pytest.mark.asyncio
+async def test_no_retry_on_ok_request():
+    retry_number = 5
+    port = 8000
+    timeout = aiohttp.ClientTimeout(connect=1, sock_read=1)
+    connector = aiohttp.TCPConnector(force_close=True)
+    session = RetryingClientSession(f'http://localhost:{port}', timeout=timeout,
+                                    max_retry_number=retry_number, base_retry_delay=0.01, connector=connector)
+    parent_request = super(type(session), session)._request
+    async_mock = mock.AsyncMock()
+    async_mock.side_effect = parent_request
+    exception = None
+    result = None
+    with get_http_server(server_handler=OkHttpHandler, server_address=('', port)):
+        with mock.patch('reportportal_client.aio.http.ClientSession._request', async_mock):
+            async with session:
+                start_time = time.time()
+                try:
+                    result = await session.get('/')
+                except Exception as exc:
+                    exception = exc
+                total_time = time.time() - start_time
+    assert exception is None
+    assert result.ok
+    assert async_mock.call_count == 1
+    assert total_time < 1
