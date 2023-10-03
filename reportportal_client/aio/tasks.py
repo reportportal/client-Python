5f9b2fca99e6054b3ddff1125a25f4d47dff13c3
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2023-10-03 22:30:35 +0300

    Fix for Python 3.7

--- reportportal_client/aio/tasks.py
+++ reportportal_client/aio/tasks.py
@@ -100,7 +100,7 @@ class BatchedTask(Generic[_T], Task[_T]):
         :param loop: Event Loop which will be used to execute the Task
         :param name: the name of the task
         """
-        if sys.version_info < (3, 7):
+        if sys.version_info < (3, 8):
             super().__init__(coro, loop=loop)
         else:
             super().__init__(coro, loop=loop, name=name)
@@ -136,7 +136,7 @@ class ThreadedTask(Generic[_T], Task[_T]):
         :param loop: Event Loop which will be used to execute the Task
         :param name: the name of the task
         """
-        if sys.version_info < (3, 7):
+        if sys.version_info < (3, 8):
             super().__init__(coro, loop=loop)
         else:
             super().__init__(coro, loop=loop, name=name)
