943c037fa8427dda76528acaec1b122c580dc6f0
Vadzim Hushchanskou <vadzim_hushchanskou@epam.com>
2026-04-09 11:41:30 +0300

    Fix pydocs

--- reportportal_client/aio/util.py
+++ reportportal_client/aio/util.py
@@ -12,6 +12,8 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 
+"""This module contains auxiliary functions for async code."""
+
 import asyncio
 from typing import Coroutine, Optional, TypeVar, Union
 
